e8f93c3917d623d11cd888f023029875717cde6c
sumona <63995737+Meherjavienothoy@users.noreply.github.com>
2022-10-05 16:35:07 +1100

    first commit

---
+++ ceillingNum.js
@@ -0,0 +1,5 @@
+//ceiling number    
+var number = 5.44;
+var roundNumber = Math.ceil(number);
+console.log(roundNumber);
+//ans 6
---
+++ floorNum.js
@@ -0,0 +1,5 @@
+//floor number    
+var number = 5.94;
+var roundNumber = Math.floor(number);
+console.log(roundNumber);
+//ans 5
